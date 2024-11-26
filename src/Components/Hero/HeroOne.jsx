7511f404320f50c15dc607885ebad91b09b73d75
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-11-26 10:26:17 +0100

    new

--- src/Components/Hero/HeroOne.jsx
+++ src/Components/Hero/HeroOne.jsx
@@ -11,10 +11,10 @@ import year from "../../Assets/Image/2025.png";
 const HeroOne = () => {
   return (
     <div
-      className="relative  max-w-full bg-center bg-no-repeat bg-cover lg:h-[800px] h-screen"
+      className="relative w-full h-screen bg-center bg-no-repeat bg-cover"
       style={{
         backgroundImage: `url(${background})`,
-        backgroundPosition: " center 20%",
+        backgroundPosition: "center 20%",
       }}
     >
       {/* Overlay */}
@@ -27,70 +27,70 @@ const HeroOne = () => {
       ></div>
 
       {/* Content */}
-      {/* Heading */}
-
-      <div className="relative mx-auto max-w-screen-2xl">
+      <div className="relative px-4 mx-auto max-w-screen-2xl md:px-8">
+        {/* Images */}
         <img
           src={solar}
           alt="Solar Panel"
-          className="absolute top-4 left-4 w-[150px] md:w-[250px] lg:w-[501.93px] lg:h-[330px]"
+          className="absolute top-[5%] left-[5%] w-[20vw] max-w-[150px] md:max-w-[250px] lg:max-w-[501px] lg:h-auto"
         />
 
         <img
           src={nigeria}
-          alt="renew nigeria"
-          className="absolute top-[130px] left-[15px] lg:top-[220px] lg:left-[171px] w-[400px] lg:w-[1068px] lg:h-[352.82px] md:top-[180px] md:left-[100px]  md:w-[568px] md:h-[252.82px] "
+          alt="Renew Nigeria"
+          className="absolute top-[20%] left-[10%] w-[80vw] max-w-[400px] md:max-w-[568px] lg:max-w-[1068px]"
         />
 
         <img
           src={gold}
-          alt="gold"
-          className="absolute lg:top-[393px] lg:left-[183px] lg:w-[1011px] lg:h-[36px] md:top-[301px] md:left-[111px] md:w-[530px] md:h-[36px] top-[197px] left-[21px] w-[375px] h-[15px]"
-        />
-        <img
-          src={goldOne}
-          alt="flash"
-          className="absolute top-[332px] left-[211.5px] lg:w-[98px] lg:h-[134px] transform rotate-[-1.6deg]"
-        />
-        <img
-          src={legi}
-          alt="Solar Panel"
-          className="absolute top-[311px] left-[1155px] lg:w-[200px] lg:h-[200px]"
-        />
-        <img
-          src={staff}
-          alt="staff"
-          className="absolute top-[383px] left-[20px] lg:w-[310px] lg:h-[406px]"
+          alt="Gold"
+          className="absolute top-[40%] left-[10%] w-[70vw] max-w-[375px] md:max-w-[530px] lg:max-w-[1011px]"
         />
 
-        <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 text-center text-white md:px-8">
+        <img
+          src={goldOne}
+          alt="Flash"
+          className="absolute top-[35%] left-[15%] w-[8vw] max-w-[98px] lg:max-w-[134px] transform rotate-[-1.6deg]"
+        />
+
+        <img
+          src={legi}
+          alt="Legislative Logo"
+          className="absolute top-[30%] right-[5%] w-[15vw] max-w-[120px] md:max-w-[160px] lg:max-w-[200px]"
+        />
+
+        <img
+          src={staff}
+          alt="Staff"
+          className="absolute bottom-[5%] left-[5%] w-[18vw] max-w-[200px] md:max-w-[260px] lg:max-w-[310px]"
+        />
+
+        {/* Text Content */}
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
           {/* Subtitle */}
-          <p className="mt-4 text-[36px] font-raleway font-bold sm:text-2xl md:text-3xl lg:mt-[580px] text-left ml-[-300px]">
+          <p className="mt-10 text-[4vw] font-raleway font-bold leading-tight lg:text-[2.5vw] md:text-[3vw] sm:text-[5vw]">
             FIRST NATIONAL LEGISLATIVE SUMMIT & EXPO ON <br /> RENEWABLE ENERGY
           </p>
 
           {/* Date and Location */}
-          <p className="mt-2 text-[32px] sm:text-xl md:text-2xl text-left ml-[-670px] font-[700] font-sanss">
+          <p className="mt-4 text-[3.5vw] lg:text-[2vw] md:text-[2.5vw] sm:text-[4vw] font-bold">
             11th – 13th February, 2025
             <br />
             Eko Hotel, Lagos State, Nigeria
           </p>
 
+          {/* Year */}
           <img
             src={year}
-            alt=""
-            className="absolute top-[599px] left-[643px] lg:w-[565.98px] lg:h-[170px]"
+            alt="Year"
+            className="mt-8 w-[50vw] max-w-[300px] md:max-w-[400px] lg:max-w-[565.98px]"
           />
 
           {/* Theme */}
-          <p className="mt-6 text-xl italic md:text-2xl lg:text-3xl font-raleway">
-            <span className="font-semibold text-[#FFC707]">Theme:</span>
-            <span className="font-bold text-white text-[36px]">
-              RENEW NIGERIA&nbsp;
-            </span>
-            <span className="font-bold text-[36px]">
-              Unlocking Nigeria's Energy Future
-            </span>
+          <p className="mt-8 text-[4vw] lg:text-[2.5vw] md:text-[3vw] sm:text-[4.5vw] italic font-raleway">
+            <span className="font-semibold text-[#FFC707]">Theme:</span>{" "}
+            <span className="font-bold text-white">RENEW NIGERIA&nbsp;</span>
+            <span className="font-bold">Unlocking Nigeria's Energy Future</span>
           </p>
         </div>
       </div>

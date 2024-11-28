41d25bcd568526870562bd7e914da3bc604282b5
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-11-28 12:24:04 +0100

    uche

--- src/Components/Hero/HeroOne.jsx
+++ src/Components/Hero/HeroOne.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import solar from "../../Assets/Image/solar.png"; // Replace with actual solar image path
 import background from "../../Assets/Image/abuja.png"; // Replace with actual background image path
-import nigeria from "../../Assets/Image/renweew.png"; // Replace with actual Nigeria image path
-import gold from "../../Assets/Image/gold.png"; // Replace with actual Nigeria image path
-import goldOne from "../../Assets/Image/goldOne.png";
+import nigeria from "../../Assets/Image/goldOn.png"; // Replace with actual Nigeria image path
+
 import legi from "../../Assets/Image/house of rep.png";
 import staff from "../../Assets/Image/staff.png";
 import year from "../../Assets/Image/2025.png";
@@ -28,58 +27,44 @@ const HeroOne = () => {
       ></div>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col items-center justify-center mx-auto">
+      <div className="relative z-10 flex flex-col items-center justify-center mx-auto max-w-screen-2xl lg:h-[330px]">
         {/* Solar Image */}
         <img
           src={solar}
           alt="Solar Panel"
-          className="absolute top-[1%] left-[-2%] w-[45vw] lg:w-[26.14vw] lg:h-[17.19vw] md:w-[35vw] md:left-[-1%]"
+          className="absolute top-[1%] left-[-2%] w-[45vw] lg:w-[501.93px] lg:h-[330px] md:w-[35vw] md:left-[-1%]"
         />
 
         {/* Nigeria Image */}
         <img
           src={nigeria}
           alt="Renew Nigeria"
-          className="absolute mt-[110%] md:mt-[35%] md:w-[70vw] md:left-[9%] lg:mt-[12%]   lg:top-[19%] 2xl:top-[19%] md:top-[170%] left-[10%] w-[79vw]  lg:w-[56vw] 2xl:left-[20%] lg:left-[19%] "
-        />
-
-        {/* Gold Image */}
-        <img
-          src={gold}
-          alt="Gold Line"
-          className="absolute hidden top-[33.2%] lg:top-[33%] left-[15.9%] w-[80%] lg:w-[59%]"
-        />
-
-        {/* GoldOne Image */}
-        <img
-          src={goldOne}
-          alt="Flash"
-          className="absolute hidden top-[30.1%] lg:top-[27.9%] left-[17.7%] lg:w-[5.10%]"
+          className="absolute mt-[110%] md:mt-[35%] md:w-[70vw] md:left-[9%] lg:mt-[-1%] lg:top-[49%] 2xl:top-[50%] md:top-[170%] left-[10%] w-[79vw]  lg:w-[900px] 2xl:w-[980px] 2xl:left-[17%] lg:left-[16%] lg:h-[306.82px] 2xl:h-[356.82px] "
         />
 
         {/* Legislative Image */}
         <img
           src={legi}
           alt="Legislative Icon"
-          className="absolute mt-[110%] right-[2%] w-[15vw] md:mt-[35%] md:w-[25vw] lg:mt-[11%] lg:w-[20vw] 2xl:top-[25%] md:top-[165%] lg:top-[14%]"
+          className="absolute mt-[110%] right-[2%] w-[15vw] md:mt-[35%] md:w-[25vw] lg:mt-[15%] lg:w-[200px] 2xl:top-[10%] md:top-[165%] lg:top-[9%]"
         />
 
         {/* Staff Image */}
         <img
           src={staff}
           alt="Staff"
-          className="absolute mt-[110%] left-[-1%] w-[24vw] md:mt-[95%] md:w-[25vw] md:left-[-4%] lg:mt-[70%]  2xl:left-[-1%] 2xl:mt-[60%] sm:left-[1%] lg:left-[1%]"
+          className="absolute mt-[110%] left-[-1%] w-[24vw] md:mt-[95%] md:w-[25vw] md:left-[-4%] lg:mt-[53%]  2xl:left-[-1%] 2xl:w-[310px] 2xl:mt-[55%] sm:left-[1%] lg:left-[1%]"
         />
 
         {/* Main Text */}
        
   
-          <p className="absolute top-[-90%] text-center p-2 text-[4vw] mt-[100%] lg:mt-[32%] 2xl:text-[2vw]   lg:ml-[17%]  font-bold leading-[1] md:text-[4vw] lg:text-[25px] lg:text-left 2xl:text-left md:top-[-190%] md:left-[2%] md:mt-[80%] 2xl:top-[-400%] lg:top-[-98%] lg:p-0 text-white ">
+          <p className="absolute top-[-90%] text-center p-2 text-[4vw] mt-[100%] lg:mt-[62%]  2xl:text-[2vw] 2xl:ml-[12.2%]  lg:ml-[16%]  font-bold leading-[1] md:text-[4vw] lg:text-[36px] lg:text-left 2xl:text-left md:top-[-190%] md:left-[2%] md:mt-[80%] 2xl:top-[-110%] lg:top-[-98%] lg:p-0 text-white font-raleway ">
             FIRST NATIONAL LEGISLATIVE SUMMIT & EXPO ON <br /> RENEWABLE ENERGY
           </p>
 
           {/* Date and Location */}
-          <p className=" absolute mt-[245%] text-[4vw]  text-white font-semibold md:text-[3vw] md:mt-[200%] lg:mt-[80%] lg:text-[2vw] lg:text-left lg:ml-[-32%] text-center">
+          <p className=" absolute mt-[245%] text-[4vw]  text-white font-semibold md:text-[3vw] md:mt-[200%] lg:mt-[70%] lg:text-[2vw] lg:text-left 2xl:ml-[-32%] lg:ml-[-33%] text-center">
             11th – 13th February, 2025
             <br />
             Eko Hotel, Lagos State, Nigeria
@@ -90,12 +75,12 @@ const HeroOne = () => {
           <img
             src={year}
             alt="2025"
-            className="absolute w-[40vw] md:mt-[140%] md:w-[35vw] lg:w-[30vw] lg:mt-[80%] lg:ml-[30%] mt-[170%]  2xl: 2xl:left-[25%] ]"
+            className="absolute w-[40vw] md:mt-[140%] md:w-[35vw] lg:w-[30vw] lg:mt-[70%] lg:ml-[30%] mt-[170%]  2xl: 2xl:left-[25%] ]"
           />
 
           {/* Theme */}
         
-          <p className="absolute  text-[4vw] lg:text-[2.5vw] md:text-[3vw] sm:text-[4.5vw] italic font-raleway mt-[280%] p-2 text-center md:mt-[220%] lg:mt-[93%] lg:ml-[7%]">
+          <p className="absolute  text-[4vw] lg:text-[2.5vw] md:text-[3vw] sm:text-[4.5vw] italic font-raleway mt-[280%] p-2 text-center md:mt-[220%] lg:mt-[85%] 2xl:mt-[90%] lg:ml-[7%]">
             <span className="font-semibold text-[#FFC707]">Theme:</span>{" "}
             <span className="font-bold text-white">RENEW NIGERIA&nbsp;</span>
             <span className="font-bold text-white ">Unlocking Nigeria's Energy Future</span>
